284f8dd864c6a1649638286d4ccdbb196435f1cd
lucas-aquino <luckasakino@protonmail.com>
2022-06-19 02:32:10 -0300

    Se agrego una prueba de conexion con el cliente en unity

--- src/server.js
+++ src/server.js
@@ -1,2 +1,11 @@
-let io = require('socket.io')(process.env.PORT || 52300);
+const io = require('socket.io')(process.env.PORT || 3600);
 
+console.log('Server started!');
+
+io.on('connection', socket => {
+    console.log('connection mode!!');
+
+    socket.on('disconnect', event => {
+        console.log('disconnected!!');
+    });
+});
